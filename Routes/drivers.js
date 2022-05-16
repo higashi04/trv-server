5a9bf6c9971041d2f4ef2507fa19a4eb813c5788
higashi04 <cesar.higashi04@gmail.com>
2022-05-16 13:59:40 -0500

    added driver routes

---
+++ Routes/drivers.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+const dbo = require('../db/conn')
+const ObjectId = require('mongodb').ObjectId
+
+
+
+router.get('/getDrivers', async(req, res) => {
+    let db_connect = dbo.getDb('trasn-vill');
+    db_connect.collection('drivers').find({}).toArray((err, result) => {
+            if(err) throw err
+            res.json(result)
+        })
+        
+})
+router.get('/getDrivers/:id', (req, res) => {
+    let db_connect = dbo.getDb();
+    let query = {_id: ObjectId(req.params.id)};
+    db_connect.collection('drivers').findOne(query, (err, result) => {
+            if (err) throw err;
+            res.json(result)
+        })
+})
+
+
+module.exports = router
--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const cors = require('cors')
 require('dotenv').config();
 
 const busRoutes = require('./Routes/buses')
+const driverRoutes = require('./Routes/drivers')
 
 app.use(cors());
 app.use(express.json());
 app.use('/buses', busRoutes);
+app.use('/drivers', driverRoutes);
+
 const port = process.env.PORT || 8083
 const dbo = require('./db/conn');
 
