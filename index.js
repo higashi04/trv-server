df5bf169a0e65a4347337d3464055e12020aad41
higashi04 <cesar.higashi04@gmail.com>
2022-05-17 16:02:00 -0500

    started login process for react apps

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ require('dotenv').config();
 
 const busRoutes = require('./Routes/buses')
 const driverRoutes = require('./Routes/drivers')
+const userRoutes = require('./Routes/users')
 
 app.use(cors());
 app.use(express.json());
 app.use('/buses', busRoutes);
 app.use('/drivers', driverRoutes);
+app.use('/users', userRoutes);
 
 const port = process.env.PORT || 8083
 const dbo = require('./db/conn');
