9f4198cae8f9cd42548ca417199cc30a9831a3b6
higashi04 <cesar.higashi04@gmail.com>
2022-05-18 09:55:53 -0500

    commit

--- Routes/users.js
+++ Routes/users.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const crypto = require('crypto')
 const dbo = require('../db/conn')
 const ObjectId = require('mongodb').ObjectId
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
 
+
+// passport.use(new LocalStrategy(function verify(username, password, cb) {
+//     let db_connect = dbo.getDb('trasn-vill');
+//     db_connect.collection('customers').find({username: })
+// }))
 
 router.post('/login', (req, res) => {
-    res.send(req.body)
+    let db_connect = dbo.getDb('trasn-vill');
+    let query = {username: req.body.username};
+    db_connect.collection('drivers').findOne(query, (err, result) => {
+            if (err) throw err;
+            res.json(result)
+        })
 })
 
 module.exports = router
