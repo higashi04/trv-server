80f2283cac237431cb1535305f4b0811e3ad8e8f
higashi04 <cesar.higashi04@gmail.com>
2022-06-08 10:28:27 -0500

    removed populate from drivers routes where i fetched many, and added it to driver findOne

--- Routes/drivers.js
+++ Routes/drivers.js
@@ -8,24 +8,24 @@ router.get('/getDrivers', async(req, res) => {
 })
 
 router.get('/getDrivers/medline', async(req, res) => {
-    const data = await drivers.find({company: 'MEDLINE', fueDadoDeBaja: false}).populate({path: "bus"}).exec()
+    const data = await drivers.find({company: 'MEDLINE', fueDadoDeBaja: false}).exec()
     res.send(data)
 })
 router.get('/getDrivers/oes', async(req, res) => {
-    const data = await drivers.find({company: 'OES', fueDadoDeBaja: false}).populate({path: "bus"}).exec()
+    const data = await drivers.find({company: 'OES', fueDadoDeBaja: false}).exec()
     res.send(data)
 })
 router.get('/getDrivers/bpi', async(req, res) => {
-    const data = await drivers.find({company: 'BPI', fueDadoDeBaja: false}).populate({path: "bus"}).exec()
+    const data = await drivers.find({company: 'BPI', fueDadoDeBaja: false}).exec()
     res.send(data)
 })
 router.get('/getDrivers/aistermi', async(req, res) => {
-    const data = await drivers.find({company: 'AISTERMI', fueDadoDeBaja: false}).populate({path: "bus"}).exec()
+    const data = await drivers.find({company: 'AISTERMI', fueDadoDeBaja: false}).exec()
     res.send(data)
 })
 
 router.get('/getDrivers/:id', async(req, res) => {
-    const data = await drivers.findById(req.params.id).exec()
+    const data = await drivers.findById(req.params.id).populate({path: "bus"}).exec()
     res.send(data)
 })
 
